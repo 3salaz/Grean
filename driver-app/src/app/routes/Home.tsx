196301682891d68ace213d34b6ecc2e30c694687
Erik Salazar <3salaz.dev@gmail.com>
2025-10-22 15:43:35 -0700

    removed driver component folder

--- driver-app/src/app/routes/Home.tsx
+++ driver-app/src/app/routes/Home.tsx
@@ -4,32 +4,25 @@ import About from "@/features/public/pages/About";
 
 const Home: React.FC = () => {
   return (
-    <PublicLayout disableScroll>
-      <div
-        className="
-          snap-y snap-mandatory
-          overflow-y-scroll
-          h-full w-full
-          scrollbar-hide
-        "
+    <div
+      className="snap-y snap-mandatory overflow-y-scroll h-full w-fullscrollbar-hide"
+    >
+      {/* LANDING */}
+      <section
+        id="landing"
+        className="section h-screen flex justify-center items-center snap-start"
       >
-        {/* LANDING */}
-        <section
-          id="landing"
-          className="section h-screen flex justify-center items-center snap-start bg-white"
-        >
-          <Landing />
-        </section>
+        <Landing />
+      </section>
 
-        {/* ABOUT */}
-        <section
-          id="about"
-          className="section h-screen flex justify-center items-center snap-start bg-[#103050]"
-        >
-          <About />
-        </section>
-      </div>
-    </PublicLayout>
+      {/* ABOUT */}
+      <section
+        id="about"
+        className="section h-screen flex justify-center items-center snap-start bg-[#103050]"
+      >
+        <About />
+      </section>
+    </div>
   );
 };
 
